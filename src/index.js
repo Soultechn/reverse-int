b96d71b78b15a385534ef4fd13060bc0e6543a27
Soultechn <Lysij64@gmail.com>
2022-01-09 17:40:10 +0400

    add: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+        let str = Math.abs(n) + '';
+        let out = '';
+        let i = str.length;
+        for (i = str.length - 1; i >= 0 ; i = i - 1) {
+          out = `${out}${str[i]}`;
+        };
+      return out
 }
